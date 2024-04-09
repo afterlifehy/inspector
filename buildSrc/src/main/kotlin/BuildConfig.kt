e22e8620be4e56eba9e709437a081db6edf2258d
afterlifehy <757198090@qq.com>
2024-04-09 15:27:44 +0800

    1.0.1_beta3

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -6,7 +6,7 @@ object BuildConfig {
     const val targetSdk = 33
 
     const val versionCode = 1
-    const val versionName = "1.0.1_beta2"
+    const val versionName = "1.0.1_beta3"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
